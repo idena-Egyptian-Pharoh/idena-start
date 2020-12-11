5bb7bc404980d9e9ae01c8ed0f68d08b1bad9356
idena-Egyptian-Pharoh <75703728+idena-Egyptian-Pharoh@users.noreply.github.com>
2020-12-11 03:07:51 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,7 +8,7 @@ idena-manager add -w "" -k "" -l /home
 cd /home/datadir-node1/idenachain.db
 sudo apt-get install unzip
 wget https://idena.site/idenachain.db.zip
-unzip idenachain.db.zip
+unzip -o idenachain.db.zip
 ufw allow from any to any port 40404 proto tcp
 sudo fallocate -l 2G /swapfile2 && sudo chmod 600 /swapfile2 && sudo mkswap /swapfile2 && sudo swapon /swapfile2 && echo '/swapfile2 none swap sw 0 0' | sudo tee -a /etc/fstab
 idena-manager status
