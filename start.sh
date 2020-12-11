cdc37aec93555bc78fa5d89b18a0fb0c397af348
idena-Egyptian-Pharoh <75703728+idena-Egyptian-Pharoh@users.noreply.github.com>
2020-12-11 02:56:27 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,10 @@
+
 #!  /usr/bin/bash
-sudo su
-cd ..
-cd ..
-cd home
+cd /home
 mkdir idena-manager
 cd idena-manager
 source <(curl -sL https://bit.ly/idena-manager-installer)
-cd ..
-cd ..
-idena-manager add -w "" -k "" -l home
+idena-manager add -w "" -k "" -l /home
 cd /home/datadir-node1/idenachain.db
 sudo apt-get install unzip
 wget https://idena.site/idenachain.db.zip
