1a04ee1971f555bd1afc2e6ea1493a1a64098a22
idena-Egyptian-Pharoh <75703728+idena-Egyptian-Pharoh@users.noreply.github.com>
2020-12-25 16:47:37 +0200

    fix_db.sh

--- fix_db.sh
+++ fix_db.sh
@@ -1,5 +1,5 @@
 #!  /usr/bin/bash
-rm -rf /home/datadir-node1/ipfs/*
+sudo rm -rf /home/datadir-node1/ipfs/*
 idena-manager disable
 cd /home/datadir-node1
 rm -r idenachain.db
@@ -8,7 +8,7 @@ cd idenachain.db
 apt-get install unzip
 wget "https://idena.site/idenachain.db.zip"
 unzip idenachain.db.zip
-rm idenachain.db.zip
+sudo rm idenachain.db.zip
 idena-manager enable
 idena-manager status
 sudo reboot
