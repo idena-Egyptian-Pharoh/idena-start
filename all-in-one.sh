712c9bc57f9d180ec730b7a1a3f3d270d524a761
idena-Egyptian-Pharoh <75703728+idena-Egyptian-Pharoh@users.noreply.github.com>
2021-01-17 00:02:57 +0200

    Create all-in-one.sh

---
+++ all-in-one.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "  _____     _                     "
+echo " |_   _|   | |                    "
+echo "   | |   __| |  ___  _ __    __ _ "
+echo "   | |  / _  | / _ \|  _ \  / _  |"
+echo "  _| |_| (_| ||  __/| | | || (_| |"
+echo " |_____|\__ _| \___||_| |_| \__ _|"
+echo
+echo "## Welcome to the Idena node installation script ##"
+echo "-------------------------------------"
+echo "https://www.idena-ar.com"
+echo "-------------------------------------"
+echo
