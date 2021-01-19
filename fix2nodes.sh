79bdc8a41812d5a8b5cffe0f4f783137046ae9ea
idena-Egyptian-Pharoh <75703728+idena-Egyptian-Pharoh@users.noreply.github.com>
2021-01-19 19:45:28 +0200

    Update fix2nodes.sh

--- fix2nodes.sh
+++ fix2nodes.sh
@@ -8,11 +8,13 @@ apt-get install unzip
 wget https://sync.idena-ar.com/idenachain.db.zip
 unzip idenachain.db.zip
 rm idenachain.db.zip
-cd /root/datadir-node2
+cd /home/datadir-node2
 rm -r idenachain.db
 mkdir idenachain.db
 cd idenachain.db
 apt-get install unzip
 wget https://sync.idena-ar.com/idenachain.db.zip
+unzip idenachain.db.zip
+rm idenachain.db.zip
 idena-manager enable
 idena-manager status
