a088e630a86b094d31a886ca0fc5eddc757c2d29
idena-Egyptian-Pharoh <75703728+idena-Egyptian-Pharoh@users.noreply.github.com>
2021-01-28 08:47:50 -0200

    Update start4nodes.sh

--- start4nodes.sh
+++ start4nodes.sh
@@ -4,6 +4,7 @@ mkdir idena-manager
 cd idena-manager
 source <(curl -sL https://bit.ly/idena-manager-installer)
 idena-manager add -w "" -k "123" -l /home
+idena-manager disable
 cd /home/datadir-node1/idenachain.db
 sudo apt-get install unzip
 wget https://sync.idena-ar.com/idenachain.db.zip
