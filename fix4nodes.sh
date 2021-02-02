f87de258a0a405943be19e5f19f0d8c7db253416
idena-Egyptian-Pharoh <75703728+idena-Egyptian-Pharoh@users.noreply.github.com>
2021-02-03 00:39:26 +0200

    Update fix4nodes.sh

--- fix4nodes.sh
+++ fix4nodes.sh
@@ -4,6 +4,9 @@ rm -rf /home/datadir-node1/ipfs/*
 rm -rf /home/datadir-node2/ipfs/*
 rm -rf /home/datadir-node3/ipfs/*
 rm -rf /home/datadir-node4/ipfs/*
+rm -rf /home/datadir-node2/idenachain.db/*
+rm -rf /home/datadir-node3/idenachain.db/*
+rm -rf /home/datadir-node4/idenachain.db/*
 cd /home/datadir-node1
 rm -r idenachain.db
 mkdir idenachain.db
@@ -11,6 +14,7 @@ cd idenachain.db
 apt-get install unzip
 wget https://sync.idena-ar.com/idenachain.db.zip
 unzip idenachain.db.zip
+rm idenachain.db.zip
 cp /home/datadir-node1/idenachain.db/* /home/datadir-node2/idenachain.db/
 cp /home/datadir-node1/idenachain.db/* /home/datadir-node3/idenachain.db/
 cp /home/datadir-node1/idenachain.db/* /home/datadir-node4/idenachain.db/
