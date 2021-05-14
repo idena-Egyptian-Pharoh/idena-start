d815022aced622b54a9bbbaf07882db4d54426f1
Idena-Egyptian-pharaoh <75703728+idena-Egyptian-Pharoh@users.noreply.github.com>
2021-05-15 01:35:12 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -10,4 +10,16 @@ unzip -o idenachain.db.zip
 sudo fallocate -l 2G /swapfile2 && sudo chmod 600 /swapfile2 && sudo mkswap /swapfile2 && sudo swapon /swapfile2 && echo '/swapfile2 none swap sw 0 0' | sudo tee -a /etc/fstab
 idena-manager status
 idena-manager restart
+sudo rm -rf /home/datadir-node1/ipfs/*
+idena-manager disable
+cd /home/datadir-node1
+rm -r idenachain.db
+mkdir idenachain.db
+cd idenachain.db
+apt-get install unzip
+wget https://sync.idena-ar.com/idenachain.db.zip
+unzip idenachain.db.zip
+sudo rm idenachain.db.zip
+idena-manager enable
+idena-manager status
 sudo reboot
