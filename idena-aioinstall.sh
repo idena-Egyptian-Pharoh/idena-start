897ca17d2dabc876a9289adfeaf4663cc6da49e5
Idena-Egyptian-pharaoh <75703728+idena-Egyptian-Pharoh@users.noreply.github.com>
2021-06-22 21:42:45 +0200

    Create idena-aioinstall.sh

---
+++ idena-aioinstall.sh
@@ -0,0 +1,12 @@
+reset
+sudo rm -r /home/idena-aio
+rm /root/main.zip
+rm /root/idena-aio.sh
+wget https://github.com/idena-Egyptian-Pharoh/idena-aio/archive/refs/heads/main.zip
+sudo apt-get install unzip
+unzip main.zip 
+mkdir /home/idena-aio
+mv /root/idena-aio-main/* /home/idena-aio
+cp /home/idena-aio/start.sh /root/idena-aio.sh
+sudo rm /root/main.zip
+sudo rm -r /root/idena-aio-main
